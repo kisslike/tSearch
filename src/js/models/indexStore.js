750d46557dc2f2b3333b465c1a3f77a0b70a0570
Anton <Feverqwe@users.noreply.github.com>
2018-03-04 07:16:13 +0500

    1

--- src/js/models/indexStore.js
+++ src/js/models/indexStore.js
@@ -7,6 +7,18 @@ const indexStore = types.model('indexStore', {
   currentProfileId: types.reference(profile),
   profiles: types.array(profile),
   trackers: types.array(tracker),
+}).actions(self => {
+  return {
+    setCurrentProfileId(id) {
+      self.currentProfileId = id;
+    }
+  };
+}).views(self => {
+  return {
+    get profile() {
+      return self.currentProfileId;
+    }
+  };
 });
 
 export default indexStore;
