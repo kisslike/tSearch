9bcc946fa2965fb6a9362922e0844c395985f4e0
Anton <anton9121@gmail.com>
2018-03-17 11:09:15 +0500

    1

--- src/js/models/trackerWorker.js
+++ src/js/models/trackerWorker.js
@@ -1,120 +1,25 @@
-import FrameWorker from '../tools/frameWorker';
-import exKitRequest from '../tools/exKitRequest';
-import exKitBuildConnectRe from '../tools/exKitBuildConnectRe';
+import PluginWorkerModel from "./pluginWorker";
 const debug = require('debug')('trackerWorker');
-const {types, getParent, isAlive} = require('mobx-state-tree');
+const {types} = require('mobx-state-tree');
 
 /**
- * @typedef {{}} TrackerWorkerM
+ * @typedef {PluginWorkerM} TrackerWorkerM
  * Model:
- * @property {string} readyState
  * Actions:
- * @property {function(string)} setReadyState
  * Views:
  * @property {function(string):Promise} search
  * @property {function(Object):Promise} searchNext
- * @property {string[]} requests
- * @property {RegExp} connectRe
- * @property {function} abortAllRequests
- * @property {function} destroyWorker
- * @property {function} afterCreate
- * @property {function} beforeDestroy
  */
 
-
-const trackerWorkerModel = types.model('trackerWorkerModel', {
-  readyState: types.optional(types.string, 'idle'), // idle, loading, ready, error
-}).actions(/**TrackerWorkerM*/self => {
-  return {
-    setReadyState(value) {
-      self.readyState = value;
-    }
-  };
-}).views(/**TrackerWorkerM*/self => {
-  let worker = null;
-  const requests = [];
-  let connectRe = null;
-  let initPromise = null;
-
-  const api = {
-    request: (details) => {
-      return exKitRequest(self, details);
-    }
-  };
-
+const trackerWorkerModel = types.compose(PluginWorkerModel).named('trackerWorkerModel').views(/**TrackerWorkerM*/self => {
   return {
     search(query) {
-      return initPromise.then(() => {
-        if (!worker) {
-          throw new Error('Worker is dead');
-        }
-        return worker.callFn('events.search', [{
-          query: query
-        }]);
-      });
+      return self.callFn('events.search', [{
+        query: query
+      }]);
     },
     searchNext(next) {
-      return initPromise.then(() => {
-        if (!worker) {
-          throw new Error('Worker is dead');
-        }
-        return worker.callFn('events.getNextPage', [next]);
-      });
-    },
-    get requests() {
-      return requests;
-    },
-    get connectRe() {
-      if (!connectRe) {
-        const tracker = getParent(self, 1);
-        connectRe = exKitBuildConnectRe(tracker.meta.connect.slice(0));
-      }
-      return connectRe;
-    },
-    abortAllRequests() {
-      requests.splice(0).forEach(req => {
-        req.abort();
-      });
-    },
-    destroyWorker() {
-      if (worker) {
-        worker.destroy();
-        worker = null;
-      }
-    },
-    afterCreate() {
-      if (!worker) {
-        self.setReadyState('loading');
-        const tracker = getParent(self, 1);
-        const trackerId = tracker.id;
-        worker = new FrameWorker({
-          trackerId: tracker.id
-        }, api);
-        initPromise = worker.callFn('init', [tracker.code, tracker.meta.require.slice(0)]).catch(() => {
-          return void 0;
-        }, err => {
-          debug('init error', trackerId, err);
-          throw new Error('InitWorkerError');
-        });
-        initPromise.then(() => {
-          if (isAlive(self)) {
-            self.setReadyState('ready');
-          } else {
-            debug('init>then skip, dead');
-          }
-        }, err => {
-          if (isAlive(self)) {
-            self.setReadyState('error');
-            self.destroyWorker();
-          } else {
-            debug('init>catch skip, dead');
-          }
-        });
-      }
-    },
-    beforeDestroy() {
-      self.destroyWorker();
-      self.abortAllRequests();
+      return self.callFn('events.getNextPage', [next]);
     }
   };
 });
